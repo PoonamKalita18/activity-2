cc587591fd122fb5ad28b582585300754ff698b1
PoonamKalita18 <arunakalita2015@gmail.com>
2024-10-10 21:48:36 +0530

    gkhdfgjk

---
+++ main.py
@@ -0,0 +1,16 @@
+a=0
+b=0
+c=0
+print(a!=b)
+print(b!=c)
+a="coding"
+b="python"
+if a!=b:
+    print("I am coding in python")
+    a=9
+    b=6
+a=int(input("enter a number"))
+if a%2==0:
+    print(a,"is an even number")
+else:
+    print(a,"is not an even number")
